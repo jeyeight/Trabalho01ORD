2aa60dec8c165008ccb374924c87e9e5318d10cb
João Pazinato <74716624+jeyeight@users.noreply.github.com>
2023-08-19 16:33:58 -0300

    Teste

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX_LINE_LENGTH 1000
 
 void ExecutaOperacoes(char *argv[]);
+void ProcessLine(char operacao, short id, char *data);
 
 int main(int argc, char *argv[]){
     if (argc == 3 && strcmp(argv[1], "-e") == 0) {
         printf("Modo de execucao de operacoes ativado ... nome do arquivo = %s\n", argv[2]);
-        
         ExecutaOperacoes(argv); 
         
 
@@ -18,52 +19,62 @@ int main(int argc, char *argv[]){
         // imprime_led();
 
     } else {
-
         fprintf(stderr, "Argumentos incorretos!\n");
         fprintf(stderr, "Modo de uso:\n");
         fprintf(stderr, "$ %s -e nome_arquivo\n", argv[0]);
         fprintf(stderr, "$ %s -p\n", argv[0]);
         exit(EXIT_FAILURE);
-
     }
 
     return 0;
 }
 
+void printLinha(char operacao, short id, char *data) {
+    printf("Operacao: %c, Id: %hd, Data: %s\n", operacao, id, data);
+}
+
+
 void ExecutaOperacoes(char *argv[]){
     FILE *arq = fopen(argv[2], "rb+");
+    short id;
     char operacao;
     short idDoRegistro;
-    char BUFFER[1000];
+    char BUFFER[1000]; //adsdasdasdsadas
+    char data[1000];
 
-    if(arq == NULL){
-        printf("Arquivo inexistente");
+    if (arq == NULL) {
+        printf("Erro ao abrir o arquivo");
         exit(1);
     }
-    
-    while(!feof(arq)){
-        BUFFER[0] = '\0';
-        operacao = fgetc(arq);
-        fseek(arq, 1, SEEK_CUR);
-        fread(BUFFER, sizeof(BUFFER), 1, arq);
-        idDoRegistro = atoi(BUFFER);
-        printf("%c", operacao);
-        printf("%d\n", idDoRegistro);
 
-        // if(!feof(arq)){
-        //     fseek(arq, 2, SEEK_CUR);
-        // }
+    while (fgets(BUFFER, sizeof(BUFFER), arq) != NULL) {
+        sscanf(BUFFER, " %c %hd|%[^\n]", &operacao, &id, data);
+        printLinha(operacao, id, data);
     }
 
     fclose(arq);
 }
 
-void ImprimeResultado(char operacao, int id, int bytes, int offset){
-    if(strcmp(operacao,"r") == 1){
-            printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (0xda)", id, bytes, offset);
-    }else if(strcmp(operacao,"i") == 1){
-            printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (0xda)", id, bytes, offset);
-    }else{
-            printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (0xda)", id, bytes, offset);
-    }
-}
+//     if(arq == NULL){
+//         printf("Arquivo inexistente");
+//         exit(1);
+//     }
+//     while ((character = fgetc(arq)) != EOF && character != '\n') {
+//             BUFFER[index] = character;
+//             index++;
+//     }
+//     printf("A linha lida do arquivo é: %s\n", BUFFER);
+//     }
+
+//     fclose(arq);
+// }
+
+// void ImprimeResultado(char operacao, int id, int bytes, int offset){
+//     if(strcmp(operacao,"r") == 1){
+//             printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, bytes, offset,&id);
+//     }else if(strcmp(operacao,"i") == 1){
+//             printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, bytes, offset,&id);
+//     }else{
+//             printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, bytes, offset, &id);
+//     }
+// }
