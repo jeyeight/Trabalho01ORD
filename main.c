5316270cfe24a42fb0bfb68ce0a738279ef8f219
João Pazinato <74716624+jeyeight@users.noreply.github.com>
2023-08-20 10:18:50 -0300

    Criação do inicio da operação de busca

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 void ExecutaOperacoes(char *argv[]);
 void ProcessLine(char operacao, short id, char *data);
+void busca_registro(short id);
 
 int main(int argc, char *argv[]){
     if (argc == 3 && strcmp(argv[1], "-e") == 0) {
@@ -34,25 +35,91 @@ void printLinha(char operacao, short id, char *data) {
 }
 
 
+
 void ExecutaOperacoes(char *argv[]){
-    FILE *arq = fopen(argv[2], "rb+");
+    FILE *arq_operacoes = fopen(argv[2], "rb+");
     short id;
     char operacao;
     short idDoRegistro;
-    char BUFFER[1000]; //adsdasdasdsadas
+    char BUFFER[1000];
     char data[1000];
 
-    if (arq == NULL) {
+    if (arq_operacoes == NULL) {
         printf("Erro ao abrir o arquivo");
         exit(1);
     }
 
-    while (fgets(BUFFER, sizeof(BUFFER), arq) != NULL) {
+    while (fgets(BUFFER, sizeof(BUFFER), arq_operacoes) != NULL) {
+        data[0] = '\0';
         sscanf(BUFFER, " %c %hd|%[^\n]", &operacao, &id, data);
         printLinha(operacao, id, data);
+
+        switch(operacao){
+            case 'r': 
+                //remove_registro();
+                break;
+            case 'i':
+                //insere_registro();
+                break;
+            case 'b':
+                busca_registro(id);
+                break;
+        }
+        
+        exit(1);
+          
     }
 
-    fclose(arq);
+    fclose(arq_operacoes);
+}
+
+void busca_registro(short id){
+    FILE *arq_dados = fopen("outros/dados.dat", "rb+");
+    short tamanho_reg;
+    char id_verificando[5];
+    short auxiliar;
+    int achou = 0;
+
+    if (arq_dados == NULL) {
+        printf("Erro ao abrir o arquivo para busca");
+        exit(1);
+    }
+
+    fseek(arq_dados, 4, SEEK_SET);
+
+    do{
+        auxiliar = 0;
+        fread(&tamanho_reg, sizeof(tamanho_reg), 1, arq_dados);
+        printf("%hd\n", tamanho_reg);
+
+        char caracter = fgetc(arq_dados);
+
+        while(caracter != EOF && caracter != '|'){
+            id_verificando[auxiliar] = caracter;
+            auxiliar++;
+
+            caracter = fgetc(arq_dados);
+
+        }
+
+        id_verificando[auxiliar] = '\0';
+
+        // printf("ID - VERIFICANDO %s\n", id_verificando);
+        // printf("TAMANHO - REG %hd\n", tamanho_reg);
+        // printf("AUXILIAR - %hd\n", auxiliar);
+        // printf("%hd\n", tamanho_reg - (auxiliar + 1)); 
+        if(id == (short)atoi(id_verificando)){
+            //Imprime_Resultado('r',);
+            achou = 1;
+            printf("ID %hd ENCONTRADO", id);
+        }else{
+            fseek(arq_dados, tamanho_reg - (auxiliar + 1), SEEK_CUR);
+        }
+    } while(achou == 0);
+
+
+    fclose(arq_dados);
+    
 }
 
 //     if(arq == NULL){
@@ -63,7 +130,7 @@ void ExecutaOperacoes(char *argv[]){
 //             BUFFER[index] = character;
 //             index++;
 //     }
-//     printf("A linha lida do arquivo é: %s\n", BUFFER);
+//     printf("A linha lida do arquivo Ã©: %s\n", BUFFER);
 //     }
 
 //     fclose(arq);
