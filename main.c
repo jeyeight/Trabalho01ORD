054e76fefa55cca809fb0861b7d07cdfb9bcf321
João Pazinato <74716624+jeyeight@users.noreply.github.com>
2023-08-22 11:06:53 -0300

    Busca e remoção funcionando sem a LED

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 void executa_operacoes(char *argv[]); //Função responsável por ler o arquivo de operações e chamar a função correspondente
 void print_linha(char operacao, short id, char *data);
 int busca_registro(short id, int print); //Função responsável pela operação de busca de registro 
-void imprime_resultado(char operacao, int id, short tamanho, int offset, char conteudo); //Função de impressão do resultado das operações
+void imprime_resultado(char operacao, short id, short tamanho, int offset, char conteudo[], int tamanho_char); //Função de impressão do resultado das operações
+void remove_registro(short id);
 
 int main(int argc, char *argv[]){
     if (argc == 3 && strcmp(argv[1], "-e") == 0) {
@@ -49,8 +50,8 @@ void executa_operacoes(char *argv[]){
     short id;
     char operacao;
     short idDoRegistro;
-    char BUFFER[1000];
-    char data[1000];
+    char BUFFER[10000];
+    char data[10000];
 
     if (arq_operacoes == NULL) {
         printf("Erro ao abrir o arquivo");
@@ -64,7 +65,7 @@ void executa_operacoes(char *argv[]){
 
         switch(operacao){
             case 'r': 
-                //remove_registro(id);
+                remove_registro(id);
                 break;
             case 'i':
                 //insere_registro();
@@ -85,70 +86,105 @@ int busca_registro(short id, int print){
     short auxiliar;
     int achou = FALSE;
     char caracter;
-    short byte_offset;
+    int byte_offset;
 
     if (arq_dados == NULL) {
         printf("Erro ao abrir o arquivo para busca");
         exit(1);
     }
-    fseek(arq_dados, 4, SEEK_SET);
-    do{
-        int count = 0;
-        auxiliar = 0;
+    if(ftell(arq_dados) == 0){
+        fseek(arq_dados, 4, SEEK_SET);
+    }
+    while((achou == FALSE) && (!feof(arq_dados))){
+        auxiliar = 0; //reiniciará para todo do while 
         fread(&tamanho_reg, sizeof(tamanho_reg), 1, arq_dados);
-        byte_offset += tamanho_reg;
+        byte_offset += (int)tamanho_reg;
         caracter = fgetc(arq_dados);
         char conteudo[2000];
 
-        while(caracter != '|' && caracter != EOF ){
+        while((caracter != '|') && (caracter != EOF)){
             id_verificando[auxiliar] = caracter;
             auxiliar++;
             caracter = fgetc(arq_dados);
         }
-        id_verificando[auxiliar] = '\0';
-        
+        id_verificando[auxiliar] = '\0'; //garantir que acaba ali, dar um fim,
+
         if(id == (short)atoi(id_verificando)){
             achou = TRUE;         
-            printf("TAMANHO:%hd\n",auxiliar); 
-            printf("%ld\n\n",ftell(arq_dados));
             fseek(arq_dados,-1,SEEK_CUR);
-            fseek(arq_dados,-auxiliar,SEEK_CUR);
+            fseek(arq_dados, -auxiliar ,SEEK_CUR);
+            int offset = ftell(arq_dados); // o + 1 acho que n precisa, pq ele começa no 0
             fread(conteudo, sizeof(char),tamanho_reg,arq_dados);
-            printf("%s \n\n",conteudo);
-            printf("%ld",ftell(arq_dados));
-            exit(1);
-            if(print == 1){
-                // imprime_resultado('b',id,tamanho_reg,ftell(arq_dados),conteudo);
+            if(conteudo[auxiliar + 1] == '*'){
+                print = 0;
+                achou = FALSE; //caso ele não saia no return -1
+                printf("\nRegistro ja foi removido.");
+                return -1;
             }
-        }else{
-            fseek(arq_dados, tamanho_reg - auxiliar, SEEK_CUR);
-            fseek(arq_dados, -1, SEEK_CUR);
-        }
-    } while(achou == FALSE && !feof(arq_dados));
+            conteudo[tamanho_reg] = '\0';
+            if(print == 1){
+                imprime_resultado('b',id,tamanho_reg,offset, conteudo, 2000);
+                id_verificando[0] = '\0';
+            }
+            
+            return offset; 
 
+        }else{
+            if(!feof(arq_dados)){
+                fseek(arq_dados, tamanho_reg - auxiliar, SEEK_CUR);
+                fseek(arq_dados, -1, SEEK_CUR);
+            }
+        id_verificando[0] = '\0';
+        }
+    }
+
+    if((achou == FALSE) || (feof(arq_dados))){
+        printf("\nNao foi encontrado esse Registro com o ID - %hd\n\n", id);
+    }
     fclose(arq_dados);
 }
 
 void remove_registro(short id){
     FILE *arq_dados = fopen("outros/dados.dat", "rb+");
+
+    if (arq_dados == NULL) {
+        printf("Erro ao abrir o arquivo para a operação de remoção.\n");
+        return;
+    }
+
+    char conteudo[2000];
+    char caracter;
     int byte_offset = busca_registro(id, FALSE);
-    short tamanho_reg; 
-    fseek(arq_dados,byte_offset,SEEK_SET);
-    fread(&tamanho_reg,sizeof(tamanho_reg),1,arq_dados);
+    short tamano_registro;
+    char tamanho_reg_char[20];
+    
+    fseek(arq_dados, byte_offset,SEEK_SET);
+    fseek(arq_dados, -2, SEEK_CUR);
+    fread(&tamano_registro, sizeof(tamano_registro), 1, arq_dados);
+    
+    short indice; 
+    char caractere = fgetc(arq_dados);
+    while(caractere != '|'){
+        caractere = fgetc(arq_dados);
+    }
+    fseek(arq_dados,ftell(arq_dados),SEEK_SET);
     fputc('*', arq_dados);
+    imprime_resultado('r', id, tamano_registro, byte_offset, conteudo, 2000);
     fclose(arq_dados);
     //insere_led(byte_offset,tamanho_reg)
 }
 
-void imprime_resultado(char operacao, int id, short tamanho, int offset, char conteudo){
+void imprime_resultado(char operacao, short id, short tamanho, int offset, char conteudo[], int tamanho_char){
+    conteudo[2000];
     switch (operacao){
         case 'r':
-            printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, tamanho, offset,&id);
+            printf("Remocao do registro de chave %hd Registro removido! (%hd bytes) Local: offset = %d bytes (%p)", id, tamanho, offset,&id);
             break;
         case 'b':
-            printf("Busca pelo registro de chave %d \n %s (%hd bytes)\nLocal: offset = %d bytes (%p)\n",id,conteudo,tamanho,offset);
+            printf("Busca pelo registro de chave %hd \n%s (%hd bytes)\nLocal: offset = %d bytes (%p)\n\n",id,conteudo,tamanho,offset);
+            break;
         case 'i':
-            printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, tamanho, offset, &id);
+            printf("Remocao do registro de chave %hd Registro removido! (%hd bytes) Local: offset = %d bytes (%p)", id, tamanho, offset, &id);
             break;
         default:
             printf("Operação não suportada");
@@ -191,3 +227,52 @@ void insere_LED(struct ListaLED* lista, int novo_registro){
 
 }
 
+struct No{
+    struct No* prox;
+    int info;
+};
+
+struct No* criar_No(int info){ 
+    struct No* no = (struct No*)malloc(sizeof(struct No));
+    no->info = info;
+    no->prox = NULL;
+    return no;
+
+}
+
+// void inicia_LED(struct ListaLED* lista){
+//     lista->tamanho = 0;
+// }
+
+// void insere_LED(struct No** cabeca, int novo_registro){
+    
+//     struct No* atual = cabeca;
+// }
+
+int lista_vazia(struct No* cabeca) {
+    return cabeca == NULL;
+}
+
+//     if(arq == NULL){
+//         printf("Arquivo inexistente");
+//         exit(1);
+//     }
+//     while ((character = fgetc(arq)) != EOF && character != '\n') {
+//             BUFFER[index] = character;
+//             index++;
+//     }
+//     printf("A linha lida do arquivo Ã©: %s\n", BUFFER);
+//     }
+
+//     fclose(arq);
+// }
+
+// void ImprimeResultado(char operacao, int id, int bytes, int offset){
+//     if(strcmp(operacao,"r") == 1){
+//             printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, bytes, offset,&id);
+//     }else if(strcmp(operacao,"i") == 1){
+//             printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, bytes, offset,&id);
+//     }else{
+//             printf("Remocao do registro de chave %d Registro removido! (%d bytes) Local: offset = %d bytes (%p)", id, bytes, offset, &id);
+//     }
+// }
